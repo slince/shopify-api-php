13190afa364cb2cf2823a96410bc3068e7070701
mazepina <putintseva@retailcrm.ru>
2021-03-17 17:41:47 +0300

    Add throw 402 status code

---
+++ src/Exception/PaymentRequiredException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the slince/shopify-api-php
+ *
+ * (c) Slince <taosikai@yeah.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Slince\Shopify\Exception;
+
+class PaymentRequiredException extends ClientException
+{
+}
